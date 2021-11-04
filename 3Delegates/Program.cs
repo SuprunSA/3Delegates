05a8e050dfc716151095edc116ee5b68acd3ef15
Супрун Сергей <4everyongg@mail.ru>
2021-11-05 02:46:33 +0400

    Add project files.

---
+++ 3Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Class1
+    {
+        delegate Class1 GetNewClass1();
+        delegate Class1 TransformClass1(Class1 class1);
+        delegate string GetClass1Description(Class1 class1, string comment);
+
+        GetNewClass1 Generator;
+        TransformClass1 Transformer;
+
+        event GetClass1Description OnDescribe;
+
+        Class1(GetClass1Description d1, GetClass1Description d2)
+        {
+            OnDescribe += d1;
+            OnDescribe += d2;
+        }
+    }
+}
